c1613eb67db2248df8106a613ad81d9c5eabc5d9
juvenal <juvenalak@gmail.com>
2023-09-26 14:26:40 +0200

    connect to sqlite

--- api/views.py
+++ api/views.py
@@ -3,19 +3,20 @@
 # Create your views here.
 from rest_framework import status
 from rest_framework.views import APIView,Response
+from .models import Item
+from .serializers import ItemSerializer
+
 
 class DumpItAPI(APIView):
     
     def get(self,request):
-        items = [
-            "apple",
-            "mango",
-            "grapes"
-        ]
-        response_data = {"datas":items}
+        items = Item.objects.all()
+        items_data = ItemSerializer(items,many=True).data
+        response_data = {"datas":items_data}
         return Response(response_data,status=status.HTTP_200_OK)
 
     def post(self,request):
         name = request.data.get('name')
-        response_data = {"name":"name" }
+        Item.objects.create(name=name)
+        response_data = {"response":"item Created"}
         return Response(response_data,status=status.HTTP_200_OK)
