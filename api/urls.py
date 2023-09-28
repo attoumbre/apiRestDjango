0317f920588f003d5b35f6c65e873d41d644924d
juvenal <juvenalak@gmail.com>
2023-09-28 08:58:00 +0200

    verb put and delete

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from .views import DumpItAPI
 urlpatterns = [
     path('', DumpItAPI.as_view()),
     path('create/',DumpItAPI.as_view()),
+    path('update/<int:id>/',DumpItAPI.as_view()),
+    path('delete/<int:id>/',DumpItAPI.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -20,3 +20,25 @@ class DumpItAPI(APIView):
         Item.objects.create(name=name)
         response_data = {"response":"item Created"}
         return Response(response_data,status=status.HTTP_200_OK)
+    
+    def put(self,request,id):
+        name = request.data.get('name')
+        item = Item.objects.filter(id=id).first()
+        if item is None:
+            response_data = {"response":"Item doesnot exists"}
+            return Response(response_data,status=status.HTTP_404_NOT_FOUND)
+        item.name = name
+        item.save()
+        response_data = {"response":"item Updated"}
+        return Response(response_data,status=status.HTTP_200_OK)
+    
+    def delete(self,request):
+        id = request.data.get('id')
+        item = Item.objects.filter(id=id).first()
+        if item is None:
+            response_data = {"response":"Item doesnot exists"}
+            return Response(response_data,status=status.HTTP_404_NOT_FOUND)
+        # item.name = name
+        item.delete()
+        response_data = {"response":"item deleted"}
+        return Response(response_data,status=status.HTTP_200_OK)
